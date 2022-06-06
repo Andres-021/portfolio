d26ad13036f11aebf424935aebb716f7786461bf
Pipe <andresfelipequintero2091@gmail.com>
2022-06-05 22:30:57 -0500

    Añadiendo modificacion de navbar, de horizontlr a vertical, ademas de implementar desplazamiento de secciones de texto en la web con respecto a la seleccion de los items del navbar.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
 import logo from './logo.svg';
 import Nav from './components/nav';
+import Section from './components/section';
 //import './App.css';
+import './css/app.css'
 
 const App = () => {
 
   return(
-    <div>
-      <div class="contenido">
-        <p>
-          Nulla facilisi. Aenean neque eros, maximus nec enim eu, dapibus tristique urna. 
-          Aenean malesuada orci sapien, eget posuere tortor ultricies sed. Nullam nec ante 
-          vel ex molestie laoreet vel ac purus. Aliquam condimentum, tortor eu condimentum 
-          lementum, quam ante dictum enim, id ultrices lacus ex ac ipsum. Praesent cursus, 
-          sapien at bibendum ultricies, elit elit rhoncus sem, ut luctus nisl odio at nulla. 
-          Cras magna tellus, pellentesque sed bibendum et, cursus eget massa. Quisque ante 
-          metus, accumsan a vulputate sed, luctus a orci. Praesent vel eros ac odio consequat 
-          tempus et ac justo. Fusce interdum sapien justo, id semper nisl euismod ac. Nulla 
-          quis lacus non ante facilisis auctor eu eu nisi. Nam id orci felis. Donec ac 
-          lacinia lacus. Nunc at euismod neque. Praesent vestibulum tellus sed purus porttitor,
-          quis dictum arcu cursus. Mauris aliquet molestie velit sit amet dignissim.
-        </p>
-      
-      </div>
+    <div className="contenido">
       <Nav/>
+      <Section
+        title="Section 1"
+        subtitle={    
+          <div>
+            <p>
+              Nulla facilisi. Aenean neque eros, maximus nec enim eu, dapibus tristique urna. 
+              Aenean malesuada orci sapien, eget posuere tortor ultricies sed. Nullam nec ante 
+              vel ex molestie laoreet vel ac purus. Aliquam condimentum, tortor eu condimentum 
+              lementum, quam ante dictum enim, id ultrices lacus ex ac ipsum. Praesent cursus, 
+              sapien at bibendum ultricies, elit elit rhoncus sem, ut luctus nisl odio at nulla. 
+              Cras magna tellus, pellentesque sed bibendum et, cursus eget massa. Quisque ante 
+              metus, accumsan a vulputate sed, luctus a orci. Praesent vel eros ac odio consequat 
+              tempus et ac justo. Fusce interdum sapien justo, id semper nisl euismod ac. Nulla 
+              quis lacus non ante facilisis auctor eu eu nisi. Nam id orci felis. Donec ac 
+              lacinia lacus. Nunc at euismod neque. Praesent vestibulum tellus sed purus porttitor,
+              quis dictum arcu cursus. Mauris aliquet molestie velit sit amet dignissim.
+            </p>
+          
+          </div>
+        }
+        dark={true}
+        id="section1"
+      />
+      
+      <Section
+        title="Section 2"
+        subtitle={    
+          <div>
+            <p>
+              Nulla facilisi. Aenean neque eros, maximus nec enim eu, dapibus tristique urna. 
+              Aenean malesuada orci sapien, eget posuere tortor ultricies sed. Nullam nec ante 
+              vel ex molestie laoreet vel ac purus. Aliquam condimentum, tortor eu condimentum 
+              lementum, quam ante dictum enim, id ultrices lacus ex ac ipsum. Praesent cursus, 
+              sapien at bibendum ultricies, elit elit rhoncus sem, ut luctus nisl odio at nulla. 
+              Cras magna tellus, pellentesque sed bibendum et, cursus eget massa. Quisque ante 
+              metus, accumsan a vulputate sed, luctus a orci. Praesent vel eros ac odio consequat 
+              tempus et ac justo. Fusce interdum sapien justo, id semper nisl euismod ac. Nulla 
+              quis lacus non ante facilisis auctor eu eu nisi. Nam id orci felis. Donec ac 
+              lacinia lacus. Nunc at euismod neque. Praesent vestibulum tellus sed purus porttitor,
+              quis dictum arcu cursus. Mauris aliquet molestie velit sit amet dignissim.
+            </p>
+          
+          </div>
+        }
+        dark={true}
+        id="section2"
+      />
+
+      <Section
+        title="Section 3"
+        subtitle={    
+          <div>
+            <p>
+              Nulla facilisi. Aenean neque eros, maximus nec enim eu, dapibus tristique urna. 
+              Aenean malesuada orci sapien, eget posuere tortor ultricies sed. Nullam nec ante 
+              vel ex molestie laoreet vel ac purus. Aliquam condimentum, tortor eu condimentum 
+              lementum, quam ante dictum enim, id ultrices lacus ex ac ipsum. Praesent cursus, 
+              sapien at bibendum ultricies, elit elit rhoncus sem, ut luctus nisl odio at nulla. 
+              Cras magna tellus, pellentesque sed bibendum et, cursus eget massa. Quisque ante 
+              metus, accumsan a vulputate sed, luctus a orci. Praesent vel eros ac odio consequat 
+              tempus et ac justo. Fusce interdum sapien justo, id semper nisl euismod ac. Nulla 
+              quis lacus non ante facilisis auctor eu eu nisi. Nam id orci felis. Donec ac 
+              lacinia lacus. Nunc at euismod neque. Praesent vestibulum tellus sed purus porttitor,
+              quis dictum arcu cursus. Mauris aliquet molestie velit sit amet dignissim.
+            </p>
+          
+          </div>
+        }
+        dark={true}
+        id="section3"
+      />
     </div>
   )
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,56 @@
+import { Link, animateScroll as scroll } from "react-scroll";
 import '../css/nav.css';
 
 
 const Nav = () => {
+
   return(
     <nav className="Nav-Content">
-      <div className="Nav-Tittle">
+      {/* <div className="Nav-Tittle">
         <div className="logo">LOGO</div>
-      </div>
-      <ul className="Nav">
+      </div> */}
+      {/* <ul className="Nav">
         <li><a href="/">Inicio</a></li>
-        <li><a href="/aboutme">Acerca de mi</a></li>
-        <li><a href="/proyects">Proyectos</a></li>
+        <li><a href="/aboutme">Sobre mi</a></li>
+        <li><a href="/projects">Proyectos</a></li>
+      </ul> */}
+      <ul className="Nav">
+        <li>
+          <Link
+            activeClass="active"
+            to="section1"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="section2"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Sobre mi
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="section3"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Proyectos
+          </Link>
+        </li>
       </ul>
     </nav>
   )
