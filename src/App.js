c49d347126196615a9b56a47981f5ce1c7ba2ca9
Andres Quintero <andresfelipequintero2091gmail.com>
2022-08-16 12:11:29 -0500

    Añadiendo cambio visual a la primera vista y fuentes de letras.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import Nav from './components/nav';
 import Section from './components/section';
 //import './App.css';
 import './css/app.css'
+import Card from './components/card';
 
 const App = () => {
 
   return(
     <div className="app-contenido">
       <Nav/>
-      <header className="app-tittle-1">
-        <h1 id="app-tittle">Andres Quintero</h1>
-      </header>
+      
+      <svg width="auto" height="auto" className='' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#29B6F6" d="M47,-63.7C62.8,-53.1,78.8,-42,86.1,-26.3C93.5,-10.7,92.1,9.4,83.5,24.4C74.9,22.4,59,49.4,43.9,59.8C28.7,70.3,14.4,81.2,0.2,80.9C-13.9,80.6,-27.8,69,-41.3,58C-54.9,47.1,-68.1,36.7,-75.4,22.5C-82.7,8.3,-84.1,-9.7,-78.1,-24.5C-72.1,-39.3,-58.7,-50.8,-44.4,-61.9C-30.2,-73,-15.1,-83.6,0.2,-83.9C15.6,-84.2,31.2,-74.3,47,-63.7Z" transform="translate(80 92)"/>
+          {/* <header className="app-tittle-1">
+            <h1 id="app-tittle">Andres Quintero</h1>
+          </header> */}
+        <text x="0.5" y="50"><img src="http://pm1.narvii.com/7119/b0abdf491cffde4bdf95850956c1b15a5591a4b5r1-712-707v2_uhq.jpg"/></text>
+        <text x="0.5" y="50" fill='white' font-size="20px">Andres Quintero</text>
+          
+      </svg>
+
       <Section
-        title="Section 1"
+        title="Sobre mi"
         subtitle={    
           <div>
             <p>
-              Nulla facilisi. Aenean neque eros, maximus nec enim eu, dapibus tristique urna. 
-              Aenean malesuada orci sapien, eget posuere tortor ultricies sed. Nullam nec ante 
-              vel ex molestie laoreet vel ac purus. Aliquam condimentum, tortor eu condimentum 
-              lementum, quam ante dictum enim, id ultrices lacus ex ac ipsum. Praesent cursus, 
-              sapien at bibendum ultricies, elit elit rhoncus sem, ut luctus nisl odio at nulla. 
-              Cras magna tellus, pellentesque sed bibendum et, cursus eget massa. Quisque ante 
-              metus, accumsan a vulputate sed, luctus a orci. Praesent vel eros ac odio consequat 
-              tempus et ac justo. Fusce interdum sapien justo, id semper nisl euismod ac. Nulla 
-              quis lacus non ante facilisis auctor eu eu nisi. Nam id orci felis. Donec ac 
-              lacinia lacus. Nunc at euismod neque. Praesent vestibulum tellus sed purus porttitor,
-              quis dictum arcu cursus. Mauris aliquet molestie velit sit amet dignissim.
+              Estudiante de Ing. De Sistemas realizando 5to semestre con conocimientos en programación y muy amante de la
+              misma, con motivación para ir aprendiendo nuevas tecnologías y abierto al cambio que las mismas ejerzan. Me gusta
+              investigar y actualizarme acerca de los nuevos temas tecnologicos que se encuentren en la actualidad.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse ut metus. Proin venenatis turpis sit amet ante consequat semper. Aenean nunc. Duis iaculis odio id lectus. Integer dapibus justo vitae elit. Nunc luctus, tortor quis iaculis tempus, urna odio iaculis erat, imperdiet lobortis orci lectus at eros. Ut a velit id odio malesuada nonummy. Aenean cursus metus a purus. Duis dapibus odio a enim. Aliquam ut diam sed nisl imperdiet gravida. Proin eget tellus ut ante dignissim dictum. Integer ut justo quis eros feugiat convallis. Praesent massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla egestas, nibh at malesuada nonummy, mi augue condimentum velit, a facilisis tortor ipsum non diam.
             </p>
           
           </div>
@@ -37,24 +42,9 @@ const App = () => {
       />
       
       <Section
-        title="Section 2"
+        title="Proyectos"
         subtitle={    
-          <div>
-            <p>
-              Nulla facilisi. Aenean neque eros, maximus nec enim eu, dapibus tristique urna. 
-              Aenean malesuada orci sapien, eget posuere tortor ultricies sed. Nullam nec ante 
-              vel ex molestie laoreet vel ac purus. Aliquam condimentum, tortor eu condimentum 
-              lementum, quam ante dictum enim, id ultrices lacus ex ac ipsum. Praesent cursus, 
-              sapien at bibendum ultricies, elit elit rhoncus sem, ut luctus nisl odio at nulla. 
-              Cras magna tellus, pellentesque sed bibendum et, cursus eget massa. Quisque ante 
-              metus, accumsan a vulputate sed, luctus a orci. Praesent vel eros ac odio consequat 
-              tempus et ac justo. Fusce interdum sapien justo, id semper nisl euismod ac. Nulla 
-              quis lacus non ante facilisis auctor eu eu nisi. Nam id orci felis. Donec ac 
-              lacinia lacus. Nunc at euismod neque. Praesent vestibulum tellus sed purus porttitor,
-              quis dictum arcu cursus. Mauris aliquet molestie velit sit amet dignissim.
-            </p>
-          
-          </div>
+          <Card/>
         }
         dark={true}
         id="section2"
---
+++ src/components/card.js
@@ -0,0 +1,19 @@
+import '../css/card.css';
+
+
+export default function Card () {
+
+  return(
+    <div class="container">
+      <div className="card">
+        <div className="imgBx">
+            <a href=""><img src="https://media.revistagq.com/photos/5ca602e93efb23a987379002/1:1/w_485,h_485,c_limit/que_significa_la_palabra_random_3683.jpg"/></a>
+        </div>
+        <div className="content">
+            <h2>PortFolio</h2>
+            <p>lorimdepsum</p>
+        </div>
+      </div>
+    </div>
+  );
+}
